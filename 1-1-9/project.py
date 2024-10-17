a44a9f55deef34536c8f8dda7dce9693ba543aee
Giovanni <giovanni.papaleo@vtsd.com>
2024-10-17 13:05:16 -0400

    my new solution to step 18

---
+++ 1-1-9/project.py
@@ -0,0 +1,17 @@
+import turtle as trtl
+trtl = trtl.Turtle()
+
+# form central galaxy
+
+trtl.pensize(10)
+trtl.penup()
+trtl.goto(0, 0)
+trtl.pendown()
+trtl.pencolor("white")
+trtl.circle(5,360)
+
+# illustrate galactic background
+
+
+
+

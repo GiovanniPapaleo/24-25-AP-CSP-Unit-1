d797c8d2e39e3f692786f000713927803e60fbe4
15076 <giovanni.papaleo@vtsd.com>
2024-10-21 11:38:03 -0400

    my new solution to step 18

--- 1-1-9/project.py
+++ 1-1-9/project.py
@@ -1,13 +1,46 @@
-import turtle as trtl
-trtl = trtl.Turtle()
+# construct dark background
 
-# form central galaxy
+import turtle
 
-trtl.penup()
-trtl.goto(0, 0)
-trtl.pendown()
+# creating turtle pen
+t = turtle.Turtle()
+t.speed(0)
+# set background origin pos
+t.penup()
+t.goto(0, -550)
+t.pendown()
+# set the fillcolor
+t.fillcolor("black")
 
-# illustrate galactic background
+# start the filling color
+t.begin_fill()
 
+# form circle
+t.circle(1100, 360)
+
+# ending the filling of the color
+t.end_fill()
+
+# create main galaxy
+
+
+
+# add and organize star positions
+t.fillcolor("white")
+t.pensize(3)
+t.penup()
+t.goto(50,0)
+t.pendown()
+for star in range(18):
+    t.begin_fill()
+    t.penup()
+    t.right(150)
+    t.forward(120)
+    t.pendown()
+    t.circle(5, 360)
+    t.end_fill()
+
+wn = turtle.Screen()
+wn.mainloop()
 
 
